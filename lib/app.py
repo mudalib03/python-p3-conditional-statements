ac13b5c75898117e2643aaa678665d210a512389
mudalib03 <mudalibabdulle17@gmail.com>
2025-05-06 10:45:57 +0300

    conditional

---
+++ lib/app.py
@@ -0,0 +1,14 @@
+print ("Hello World!", end=" ")
+print("Hello sun!", end="!! " )
+print("Hello sky!", end="!!!\n")
+
+
+age = 22
+print("you are  {age} years old")
+
+x = 10
+y = 2
+
+x = x/y
+
+print(x/y)
